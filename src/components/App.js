8e577c96544be6afac6b652156de4c168b016d13
Brian Bycynski <high5bri@hotmail.com>
2017-04-22 07:25:08 -0400

    started new portfolio

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import ReactRouter from './ReactRouter'
+import { Image } from 'react-bootstrap'
 import '../styles/cssgrid.scss'
 
 export default class App extends Component {
@@ -23,14 +25,50 @@ export default class App extends Component {
   render () {
     return <div className='wrapper'>
       <div className='box header'>
-        <div className='head1'>
+        <div>
           <h1>{this.state.name}</h1>
         </div>
+        <div>
+          <Image src={this.state.imageURL} responsive circle />
+        </div>
+        <p>{this.state.bio}</p>
+        <p>I'm a Jr, Front End Engineer in {this.state.location}</p>
       </div>
-      <div className='box sidebar'>Sidebar</div>
-      <div className='box sidebar2'>Sidebar 2</div>
-      <div className='box content'>Content</div>
-      <div className='box footer'>Footer</div>
+      <div className='box sidebar'>
+        <p>Some Of The Skills I Learned At <br /> <img src='https://www.theironyard.com/etc/designs/theironyard/icons/iron-yard-logo.svg' alt='' /></p>
+        <div className='skills'><ul>
+          <li><Image src='https://cdn1.iconfinder.com/data/icons/line-essentials-64/20/3155-128.png' alt='' responsive /></li>
+          <li><Image src='https://cdn0.iconfinder.com/data/icons/website-kit-2/512/icon_287-128.png' alt='' responsive /></li>
+        </ul>
+          <ul>
+            <li><Image src='https://cdn0.iconfinder.com/data/icons/long-shadow-web-icons/512/boostrap-128.png' responsive /></li>
+            <li><Image src='https://cdn4.iconfinder.com/data/icons/scripting-and-programming-languages/512/js-128.png' alt='' responsive /></li>
+          </ul>
+          <ul>
+            <li><Image src='https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-128.png' alt='' responsive /></li>
+            <li><Image src='https://cdn0.iconfinder.com/data/icons/free_windows7_icons_emoticons/64/clown.png' responsive /></li>
+          </ul></div>
+      </div>
+      <div className='box sidebar2'>
+        <p>React Router</p>
+        <ReactRouter />
+      </div>
+      <div className='box content'>
+        <a href='https://twitter.com/Bdoggy72'>
+          <Image src='https://cdn1.iconfinder.com/data/icons/social-media-icon-1/112/twitter-128.png' responsive />
+        </a>
+        <p>My Current GitHub Chart</p>
+        <div>
+          <a href='https://github.com/bdog72'>
+            <Image src='http://ghchart.rshah.org/bdog72' alt='Brians GitHub chart' responsive />
+          </a>
+        </div>
+      </div>
+      <div className='box footer'>
+        <p>813-505-4733</p>
+        <p>high5bri&#64;hotmail.com</p>
+        <p>Made With &hearts; From What I Learned At The Iron Yard</p>
+      </div>
     </div>
   }
 }
---
+++ src/components/ReactRouter.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+
+export default class ReactRouter extends Component {
+  render () {
+    const Example1 = () => (
+      <div>
+        <h2>&spades;</h2>
+      </div>
+    )
+    const Example2 = () => (
+      <div>
+        <h2>&clubs;</h2>
+      </div>
+    )
+    const Example3 = () => (
+      <div>
+        <h2>&hearts;</h2>
+      </div>
+    )
+    const Example4 = () => (
+      <div>
+        <h2>&diams;</h2>
+      </div>
+    )
+    const BasicExample = () => (
+      <Router>
+        <div>
+          <ul>
+            <li><Link to='/'><h2>SPADES</h2></Link></li>
+            <li><Link to='/example2'><h2>CLUBS</h2></Link></li>
+            <li><Link to='/example3'><h2>HEARTS</h2></Link></li>
+            <li><Link to='/example4'><h2>DIAMONDS</h2></Link></li>
+          </ul>
+          <hr />
+          <Route exact path='/' component={Example1} />
+          <Route path='/example2' component={Example2} />
+          <Route path='/example3' component={Example3} />
+          <Route path='/example4' component={Example4} />
+        </div>
+      </Router>
+    )
+    return <div>
+      <BasicExample />
+    </div>
+  }
+}
